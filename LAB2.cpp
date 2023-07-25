66a6ea5e143753fa0d0b7042847a3350e5d1d946
Nhan <nhan121001@gmail.com>
2023-07-25 16:49:25 +0700

    Add new project Nhan

--- LAB2.cpp
+++ LAB2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <iomanip>
 using namespace std;
 //5
 int demSoLuongChuSo(int n) {
@@ -162,6 +163,99 @@ string phanLoai(double diemTrungBinh) {
     } else {
         return "Kem";
     }
+}
+//1 Ứng dụng
+void inBangMaASCII(int trang) {
+    int kyTuBatDau = (trang - 1) * 22;
+    int kyTuKetThuc = trang * 22 - 1;
+
+    cout << "Ma\tKy tu" << endl;
+    for (int i = kyTuBatDau; i <= kyTuKetThuc && i <= 127; i++) {
+        cout << i << "\t" << (char)i << endl;
+    }
+}
+//2 Ứng dụng
+// Hàm tính giai thừa
+int tinhGiaiThua(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    int giaiThua = 1;
+    for (int i = 1; i <= n; i++) {
+        giaiThua *= i;
+    }
+    return giaiThua;
+}
+
+// Hàm tính tổng S(n)
+double tinhTong(int n) {
+    double tong = 0.0;
+    double temp = 0.0;
+    for (int i = 1; i <= n; i++) {
+        temp += i;
+        tong += temp / tinhGiaiThua(i);
+    }
+    return tong;
+}
+//3 Ứng dụng 
+void phuongAnRutTien(int n) {
+    int soTien100000 = n / 100000;
+    n %= 100000;
+
+    int soTien50000 = n / 50000;
+    n %= 50000;
+
+    int soTien20000 = n / 20000;
+    n %= 20000;
+
+    int soTien10000 = n / 10000;
+    n %= 10000;
+
+    cout << "So to tien 100,000 VND: " << soTien100000 << endl;
+    cout << "So to tien 50,000 VND: " << soTien50000 << endl;
+    cout << "So to tien 20,000 VND: " << soTien20000 << endl;
+    cout << "So to tien 10,000 VND: " << soTien10000 << endl;
+}
+//4 Ứng dụng 
+// Hàm tính tiền thưởng dựa trên số ngày công
+double tinhTienThuong(int soNgayCong, double luongCoBan) {
+    double tienThuong = 0.0;
+    if (soNgayCong >= 25) {
+        tienThuong = luongCoBan * 0.2;
+    } else if (soNgayCong >= 22) {
+        tienThuong = luongCoBan * 0.1;
+    }
+    return tienThuong;
+}
+
+// Hàm tính tổng thu nhập của nhân viên
+double tinhTongThuNhap(double luongCoBan, int soNgayCong, string chucVu) {
+    double tienThuong = tinhTienThuong(soNgayCong, luongCoBan);
+    double phuCap = 0.0;
+    if (chucVu == "GD") {
+        phuCap = 250000;
+    } else if (chucVu == "PGD") {
+        phuCap = 200000;
+    } else if (chucVu == "TP") {
+        phuCap = 180000;
+    } else if (chucVu == "NV") {
+        phuCap = 150000;
+    }
+    return luongCoBan * soNgayCong + tienThuong + phuCap;
+}
+//5 Ứng dụng 
+// Hàm tính điểm ưu tiên dựa trên khu vực
+double tinhDiemUuTien(string maThiSinh) {
+    double diemUuTien = 0.0;
+    string khuVuc = maThiSinh.substr(0, 3);
+    if (khuVuc == "KV1") {
+        diemUuTien = 0.5;
+    } else if (khuVuc == "KV2") {
+        diemUuTien = 1.0;
+    } else if (khuVuc == "KV3") {
+        diemUuTien = 2.5;
+    }
+    return diemUuTien;
 }
 int main()
 {
@@ -386,12 +480,183 @@ int main()
         cout << "Diem trung binh cua hoc sinh " << hoTen << " la: " << dtb << endl;
         cout << "Phan loai cua hoc sinh " << hoTen << " la: " << phanLoai(dtb) << endl;
 
+        
+    }
+     }
+        break;
+            case 11 :{
+            int soTrang = 1;
+        char ch;
+        do {
+            inBangMaASCII(soTrang);
+
+            cout << "Nhan Enter de xem trang ke tiep, nhan 'q' de thoat." << endl;
+            ch = cin.get();
+            if (ch == '\n') {
+                soTrang++;
+            }
+        } while (ch != 'q');
+
         return 0;
     }
+        break;
+        case 12: 
+        {
+             int n;
+    cout << "Nhap n: ";
+    cin >> n;
+
+    double ketQua = tinhTong(n);
+    cout << "S(" << n << ") = " << ketQua << endl;
+
+    return 0;
+    }
+    
+        break;
+        case 13 : 
+        {
+                int n;
+        cout << "Nhap so tien can rut (chia het cho 10,000 VND): ";
+        cin >> n;
+
+        if (n % 10000 != 0) {
+            cout << "So tien nhap vao khong hop le!" << endl;
+        } else {
+            phuongAnRutTien(n);
         }
+
+        
+    }
+        
+        break;
+        case 14 : {
+          int maNhanVien;
+        string hoTen, chucVu;
+        double luongCoBan;
+        int soNgayCong;
+
+        cout << "Nhap ma nhan vien: NV";
+        cin >> maNhanVien;
+
+        cout << "Nhap ho ten: ";
+        cin.ignore();
+        getline(cin, hoTen);
+
+        cout << "Nhap luong co ban: ";
+        cin >> luongCoBan;
+
+        cout << "Nhap so ngay cong: ";
+        cin >> soNgayCong;
+
+        cout << "Nhap chuc vu (GD, PGD, TP, NV): ";
+        cin.ignore();
+        getline(cin, chucVu);
+
+        double tongThuNhap = tinhTongThuNhap(luongCoBan, soNgayCong, chucVu);
+
+        cout << "\nThong tin nhan vien:" << endl;
+        cout << "Ma nhan vien: NV" << setw(2) << setfill('0') << maNhanVien << endl;
+        cout << "Ho ten: " << hoTen << endl;
+        cout << "Luong co ban: " << luongCoBan << " VND" << endl;
+        cout << "So ngay cong: " << soNgayCong << " ngay" << endl;
+        cout << "Chuc vu: " << chucVu << endl;
+        cout << "Tong thu nhap: " << tongThuNhap << " VND" << endl;
+
+        return 0;
+    }
+        
+        break;
+        case 15:
+        {
+            string maThiSinh, hoTen;
+        double diemToan, diemLy, diemHoa;
+
+        // Nhập thông tin thí sinh
+        cout << "Nhap ma thi sinh: ";
+        cin >> maThiSinh;
+
+        cin.ignore(); // Loại bỏ kí tự '\n' trong bộ nhớ đệm
+
+        cout << "Nhap ho ten: ";
+        getline(cin, hoTen);
+
+        cout << "Nhap diem Toan: ";
+        cin >> diemToan;
+
+        cout << "Nhap diem Ly: ";
+        cin >> diemLy;
+
+        cout << "Nhap diem Hoa: ";
+        cin >> diemHoa;
+
+        // Nhập khu vực ưu tiên
+        double diemUuTien = 0.0;
+        cout << "Nhap khu vuc uu tien (KV1, KV2, KV3): ";
+        string khuVuc;
+        cin >> khuVuc;
+        diemUuTien = tinhDiemUuTien(khuVuc);
+
+        // Tính tổng điểm
+        double tongDiem = diemToan * 2 + diemLy + diemHoa + diemUuTien;
+
+        // Xác định trạng thái
+        string trangThai = (tongDiem >= 24) ? "TRUNG TUYEN" : "TRUOT";
+
+        // In kết quả
+        cout << "\nThong tin thi sinh:" << endl;
+        cout << "Ma thi sinh: " << maThiSinh << endl;
+        cout << "Ho ten: " << hoTen << endl;
+        cout << "Diem uu tien: " << diemUuTien << endl;
+        cout << "Tong diem: " << tongDiem << endl;
+        cout << "Trang thai: " << trangThai << endl;
+
+        return 0;
+    }
+        
+        break;
+        case 16 : {
+            int N;
+        cout << "Nhap so luong con so N: ";
+        cin >> N;
+
+        int A[100];
+        cout << "Nhap day tang dan A: ";
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+
+        int maxNum = A[N - 1]; // Số lớn nhất trong dãy ban đầu
+
+        // Tạo một mảng bool để đánh dấu các số đã xuất hiện trong dãy
+        bool isPresent[200] = {false};
+
+        // Đánh dấu các số đã xuất hiện trong dãy
+        for (int i = 0; i < N; i++) {
+            isPresent[A[i]] = true;
+        }
+
+        // Liệt kê các số còn thiếu trong khoảng từ 1 đến số lớn nhất
+        bool isMissing = false;
+        for (int i = 1; i <= maxNum; i++) {
+            if (!isPresent[i]) {
+                isMissing = true;
+                cout << i << endl;
+            }
+        }
+
+        // Kiểm tra và in kết quả
+        if (!isMissing) {
+            cout << "Excellent! Excellent!" << endl;
+        }
+
+        return 0;
+    }
+        
+        break;
         default: {
             cout << "khong co bai ban chon" << endl;
             break;
         }
     }
 }
+
